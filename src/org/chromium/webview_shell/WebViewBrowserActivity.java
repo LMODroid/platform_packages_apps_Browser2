9aa1af8ab126fb6a364291c259a5a37532e8f0ef
Mikhail Naganov <mnaganov@google.com>
2016-01-22 14:09:18 -0800

    Update Browser with the current code from the Chromium tree
    Synced WebViewBrowserActivity, manifest and resources up to
    the state of the Chromium tree at refs/heads/master@{#371018}
    
    Differences from the Chromium version:
    
      -- WebViewBrowserActivity uses android.util.Log instead of
         org.chromium.base.Log, thus difference in call parameters;
    
      -- relative class names are used for Activity classes
         in the manifest file.
    
    Change-Id: I9af67200e82c17d76347a1ca4ffb060be3e777bf

--- src/org/chromium/webview_shell/WebViewBrowserActivity.java
+++ src/org/chromium/webview_shell/WebViewBrowserActivity.java
@@ -4,18 +4,30 @@
 
 package org.chromium.webview_shell;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Browser;
+import android.util.Log;
+import android.util.SparseArray;
 
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnKeyListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.view.inputmethod.InputMethodManager;
 
 import android.webkit.GeolocationPermissions;
@@ -26,7 +38,6 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import android.widget.EditText;
-import android.widget.LinearLayout.LayoutParams;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
@@ -36,6 +47,9 @@ import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -45,67 +59,107 @@ import java.util.regex.Pattern;
  * on top of the webview for manually specifying URLs to load.
  */
 public class WebViewBrowserActivity extends Activity implements PopupMenu.OnMenuItemClickListener {
-    private EditText mUrlBar;
-    private WebView mWebView;
-    private String mWebViewVersion;
+    private static final String TAG = "WebViewShell";
+
+    // Our imaginary Android permission to associate with the WebKit geo permission
+    private static final String RESOURCE_GEO = "RESOURCE_GEO";
+    // Our imaginary WebKit permission to request when loading a file:// URL
+    private static final String RESOURCE_FILE_URL = "RESOURCE_FILE_URL";
+    // WebKit permissions with no corresponding Android permission can always be granted
+    private static final String NO_ANDROID_PERMISSION = "NO_ANDROID_PERMISSION";
+
+    // Map from WebKit permissions to Android permissions
+    private static final HashMap<String, String> sPermissions;
+    static {
+        sPermissions = new HashMap<String, String>();
+        sPermissions.put(RESOURCE_GEO, Manifest.permission.ACCESS_FINE_LOCATION);
+        sPermissions.put(RESOURCE_FILE_URL, Manifest.permission.READ_EXTERNAL_STORAGE);
+        sPermissions.put(PermissionRequest.RESOURCE_AUDIO_CAPTURE,
+                Manifest.permission.RECORD_AUDIO);
+        sPermissions.put(PermissionRequest.RESOURCE_MIDI_SYSEX, NO_ANDROID_PERMISSION);
+        sPermissions.put(PermissionRequest.RESOURCE_PROTECTED_MEDIA_ID, NO_ANDROID_PERMISSION);
+        sPermissions.put(PermissionRequest.RESOURCE_VIDEO_CAPTURE,
+                Manifest.permission.CAMERA);
+    }
 
     private static final Pattern WEBVIEW_VERSION_PATTERN =
             Pattern.compile("(Chrome/)([\\d\\.]+)\\s");
 
-    // TODO(michaelbai) : Replace "android.webkit.resoruce.MIDI_SYSEX" with
-    // PermissionRequest.RESOURCE_MIDI_SYSEX once Android M SDK is used.
-    private static final String[] AUTOMATICALLY_GRANT =
-            { PermissionRequest.RESOURCE_VIDEO_CAPTURE, PermissionRequest.RESOURCE_AUDIO_CAPTURE,
-              "android.webkit.resource.MIDI_SYSEX" };
+    private EditText mUrlBar;
+    private WebView mWebView;
+    private String mWebViewVersion;
+
+    // Each time we make a request, store it here with an int key. onRequestPermissionsResult will
+    // look up the request in order to grant the approprate permissions.
+    private SparseArray<PermissionRequest> mPendingRequests = new SparseArray<PermissionRequest>();
+    private int mNextRequestKey = 0;
+
+    // Work around our wonky API by wrapping a geo permission prompt inside a regular
+    // PermissionRequest.
+    private static class GeoPermissionRequest extends PermissionRequest {
+        private String mOrigin;
+        private GeolocationPermissions.Callback mCallback;
+
+        public GeoPermissionRequest(String origin, GeolocationPermissions.Callback callback) {
+            mOrigin = origin;
+            mCallback = callback;
+        }
+
+        public Uri getOrigin() {
+            return Uri.parse(mOrigin);
+        }
+
+        public String[] getResources() {
+            return new String[] { WebViewBrowserActivity.RESOURCE_GEO };
+        }
+
+        public void grant(String[] resources) {
+            assert resources.length == 1;
+            assert WebViewBrowserActivity.RESOURCE_GEO.equals(resources[0]);
+            mCallback.invoke(mOrigin, true, false);
+        }
+
+        public void deny() {
+            mCallback.invoke(mOrigin, false, false);
+        }
+    }
+
+    // For simplicity, also treat the read access needed for file:// URLs as a regular
+    // PermissionRequest.
+    private class FilePermissionRequest extends PermissionRequest {
+        private String mOrigin;
+
+        public FilePermissionRequest(String origin) {
+            mOrigin = origin;
+        }
+
+        public Uri getOrigin() {
+            return Uri.parse(mOrigin);
+        }
+
+        public String[] getResources() {
+            return new String[] { WebViewBrowserActivity.RESOURCE_FILE_URL };
+        }
+
+        public void grant(String[] resources) {
+            assert resources.length == 1;
+            assert WebViewBrowserActivity.RESOURCE_FILE_URL.equals(resources[0]);
+            // Try again now that we have read access.
+            WebViewBrowserActivity.this.mWebView.loadUrl(mOrigin);
+        }
+
+        public void deny() {
+            // womp womp
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_webview_browser);
-        mWebView = (WebView) findViewById(R.id.webview);
-        WebSettings settings = mWebView.getSettings();
-        initializeSettings(settings);
-
-        Matcher matcher = WEBVIEW_VERSION_PATTERN.matcher(settings.getUserAgentString());
-        if (matcher.find()) {
-            mWebViewVersion = matcher.group(2);
-        } else {
-            mWebViewVersion = "-";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
         }
-        setTitle(getResources().getString(R.string.title_activity_browser) + " " + mWebViewVersion);
-
-        mWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
-                return false;
-            }
-
-            @Override
-            public void onReceivedError(WebView view, int errorCode, String description,
-                    String failingUrl) {
-                setUrlFail(true);
-            }
-        });
-
-        mWebView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public Bitmap getDefaultVideoPoster() {
-                return Bitmap.createBitmap(
-                        new int[] {Color.TRANSPARENT}, 1, 1, Bitmap.Config.ARGB_8888);
-            }
-
-            @Override
-            public void onGeolocationPermissionsShowPrompt(String origin,
-                    GeolocationPermissions.Callback callback) {
-                callback.invoke(origin, true, false);
-            }
-
-            @Override
-            public void onPermissionRequest(PermissionRequest request) {
-                request.grant(AUTOMATICALLY_GRANT);
-            }
-        });
-
+        setContentView(R.layout.activity_webview_browser);
         mUrlBar = (EditText) findViewById(R.id.url_field);
         mUrlBar.setOnKeyListener(new OnKeyListener() {
             public boolean onKey(View view, int keyCode, KeyEvent event) {
@@ -117,12 +171,155 @@ public class WebViewBrowserActivity extends Activity implements PopupMenu.OnMenu
             }
         });
 
+        createAndInitializeWebView();
+
         String url = getUrlFromIntent(getIntent());
         if (url != null) {
             setUrlBarText(url);
             setUrlFail(false);
             loadUrlFromUrlBar(mUrlBar);
         }
+    }
+
+    ViewGroup getContainer() {
+        return (ViewGroup) findViewById(R.id.container);
+    }
+
+    private void createAndInitializeWebView() {
+        WebView webview = new WebView(this);
+        WebSettings settings = webview.getSettings();
+        initializeSettings(settings);
+
+        Matcher matcher = WEBVIEW_VERSION_PATTERN.matcher(settings.getUserAgentString());
+        if (matcher.find()) {
+            mWebViewVersion = matcher.group(2);
+        } else {
+            mWebViewVersion = "-";
+        }
+        setTitle(getResources().getString(R.string.title_activity_browser) + " " + mWebViewVersion);
+
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                setUrlBarText(url);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                setUrlBarText(url);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                // "about:" and "chrome:" schemes are internal to Chromium;
+                // don't want these to be dispatched to other apps.
+                if (url.startsWith("about:") || url.startsWith("chrome:")) {
+                    return false;
+                }
+                return startBrowsingIntent(WebViewBrowserActivity.this, url);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description,
+                    String failingUrl) {
+                setUrlFail(true);
+            }
+        });
+
+        webview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public Bitmap getDefaultVideoPoster() {
+                return Bitmap.createBitmap(
+                        new int[] {Color.TRANSPARENT}, 1, 1, Bitmap.Config.ARGB_8888);
+            }
+
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin,
+                    GeolocationPermissions.Callback callback) {
+                onPermissionRequest(new GeoPermissionRequest(origin, callback));
+            }
+
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                WebViewBrowserActivity.this.requestPermissionsForPage(request);
+            }
+        });
+
+        mWebView = webview;
+        getContainer().addView(
+                webview, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        setUrlBarText("");
+    }
+
+    // WebKit permissions which can be granted because either they have no associated Android
+    // permission or the associated Android permission has been granted
+    private boolean canGrant(String webkitPermission) {
+        String androidPermission = sPermissions.get(webkitPermission);
+        if (androidPermission == NO_ANDROID_PERMISSION) {
+            return true;
+        }
+        return PackageManager.PERMISSION_GRANTED == checkSelfPermission(androidPermission);
+    }
+
+    private void requestPermissionsForPage(PermissionRequest request) {
+        // Deny any unrecognized permissions.
+        for (String webkitPermission : request.getResources()) {
+            if (!sPermissions.containsKey(webkitPermission)) {
+                Log.w(TAG, "Unrecognized WebKit permission: " + webkitPermission);
+                request.deny();
+                return;
+            }
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            request.grant(request.getResources());
+            return;
+        }
+
+        // Find what Android permissions we need before we can grant these WebKit permissions.
+        ArrayList<String> androidPermissionsNeeded = new ArrayList<String>();
+        for (String webkitPermission : request.getResources()) {
+            if (!canGrant(webkitPermission)) {
+                // We already checked for unrecognized permissions, and canGrant will skip over
+                // NO_ANDROID_PERMISSION cases, so this is guaranteed to be a regular Android
+                // permission.
+                String androidPermission = sPermissions.get(webkitPermission);
+                androidPermissionsNeeded.add(androidPermission);
+            }
+        }
+
+        // If there are no such Android permissions, grant the WebKit permissions immediately.
+        if (androidPermissionsNeeded.isEmpty()) {
+            request.grant(request.getResources());
+            return;
+        }
+
+        // Otherwise, file a new request
+        if (mNextRequestKey == Integer.MAX_VALUE) {
+            Log.e(TAG, "Too many permission requests");
+            return;
+        }
+        int requestCode = mNextRequestKey;
+        mNextRequestKey++;
+        mPendingRequests.append(requestCode, request);
+        requestPermissions(androidPermissionsNeeded.toArray(new String[0]), requestCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+            String permissions[], int[] grantResults) {
+        // Verify that we can now grant all the requested permissions. Note that although grant()
+        // takes a list of permissions, grant() is actually all-or-nothing. If there are any
+        // requested permissions not included in the granted permissions, all will be denied.
+        PermissionRequest request = mPendingRequests.get(requestCode);
+        for (String webkitPermission : request.getResources()) {
+            if (!canGrant(webkitPermission)) {
+                request.deny();
+                return;
+            }
+        }
+        request.grant(request.getResources());
+        mPendingRequests.delete(requestCode);
     }
 
     public void loadUrlFromUrlBar(View view) {
@@ -153,6 +350,15 @@ public class WebViewBrowserActivity extends Activity implements PopupMenu.OnMenu
     @Override
     public boolean onMenuItemClick(MenuItem item) {
         switch(item.getItemId()) {
+            case R.id.menu_reset_webview:
+                if (mWebView != null) {
+                    ViewGroup container = getContainer();
+                    container.removeView(mWebView);
+                    mWebView.destroy();
+                    mWebView = null;
+                }
+                createAndInitializeWebView();
+                return true;
             case R.id.menu_about:
                 about();
                 hideKeyboard(mUrlBar);
@@ -206,6 +412,17 @@ public class WebViewBrowserActivity extends Activity implements PopupMenu.OnMenu
     }
 
     private void loadUrl(String url) {
+        // Request read access if necessary
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && "file".equals(Uri.parse(url).getScheme())
+                && PackageManager.PERMISSION_DENIED
+                        == checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            requestPermissionsForPage(new FilePermissionRequest(url));
+        }
+
+        // If it is file:// and we don't have permission, they'll get the "Webpage not available"
+        // "net::ERR_ACCESS_DENIED" page. When we get permission, FilePermissionRequest.grant()
+        // will reload.
         mWebView.loadUrl(url);
         mWebView.requestFocus();
     }
@@ -231,5 +448,74 @@ public class WebViewBrowserActivity extends Activity implements PopupMenu.OnMenu
 
     private static String getUrlFromIntent(Intent intent) {
         return intent != null ? intent.getDataString() : null;
+    }
+
+    static final Pattern BROWSER_URI_SCHEMA = Pattern.compile(
+            "(?i)"   // switch on case insensitive matching
+            + "("    // begin group for schema
+            + "(?:http|https|file):\\/\\/"
+            + "|(?:inline|data|about|chrome|javascript):"
+            + ")"
+            + "(.*)");
+
+    private static boolean startBrowsingIntent(Context context, String url) {
+        Intent intent;
+        // Perform generic parsing of the URI to turn it into an Intent.
+        try {
+            intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
+        } catch (Exception ex) {
+            Log.w(TAG, "Bad URI " + url, ex);
+            return false;
+        }
+        // Check for regular URIs that WebView supports by itself, but also
+        // check if there is a specialized app that had registered itself
+        // for this kind of an intent.
+        Matcher m = BROWSER_URI_SCHEMA.matcher(url);
+        if (m.matches() && !isSpecializedHandlerAvailable(context, intent)) {
+            return false;
+        }
+        // Sanitize the Intent, ensuring web pages can not bypass browser
+        // security (only access to BROWSABLE activities).
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setComponent(null);
+        Intent selector = intent.getSelector();
+        if (selector != null) {
+            selector.addCategory(Intent.CATEGORY_BROWSABLE);
+            selector.setComponent(null);
+        }
+
+        // Pass the package name as application ID so that the intent from the
+        // same application can be opened in the same tab.
+        intent.putExtra(Browser.EXTRA_APPLICATION_ID, context.getPackageName());
+        try {
+            context.startActivity(intent);
+            return true;
+        } catch (ActivityNotFoundException ex) {
+            Log.w(TAG, "No application can handle " + url);
+        }
+        return false;
+    }
+
+    /**
+     * Search for intent handlers that are specific to the scheme of the URL in the intent.
+     */
+    private static boolean isSpecializedHandlerAvailable(Context context, Intent intent) {
+        PackageManager pm = context.getPackageManager();
+        List<ResolveInfo> handlers = pm.queryIntentActivities(intent,
+                PackageManager.GET_RESOLVED_FILTER);
+        if (handlers == null || handlers.size() == 0) {
+            return false;
+        }
+        for (ResolveInfo resolveInfo : handlers) {
+            if (!isNullOrGenericHandler(resolveInfo.filter)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNullOrGenericHandler(IntentFilter filter) {
+        return filter == null
+                || (filter.countDataAuthorities() == 0 && filter.countDataPaths() == 0);
     }
 }
